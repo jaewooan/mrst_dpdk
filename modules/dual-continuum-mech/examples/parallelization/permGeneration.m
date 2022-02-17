5eac2d8819aead8d704a99dab244da73d20461cb
jaewooan <jaewooan@stanford.edu>
2022-02-16 22:50:07 -0800

    modificaiton for parallization

---
+++ modules/dual-continuum-mech/examples/parallelization/permGeneration.m
@@ -0,0 +1,65 @@
+clear
+clc
+close all
+addpath('../SGS/')
+
+%% Input from simulation: please check if this dimension is consistent with the values in the simulation!
+Lx = 100; Ly = 100;
+nx = 50; ny = 50; % check 
+nCase = 10000; % number of realization
+nParallel = 10;
+
+%% SGS Setting
+covar.model = 'gaussian'; % type of covariance function, see functions/kriginginitiaite.m for option
+covar.range0 = [nx/10 nx/10]; % range of covariance [d_nx d_ny]
+covar.azimuth = 0; % orientation of the covariance
+covar.c0 = 1; % variance
+covar.alpha = 1; % parameter of covariance function (facult)
+parm.saveit = false; % save in a file
+parm.seed_path = 'shuffle'; % seed for the path
+parm.seed_search = 'shuffle'; % seed for the search (equal distance node)
+parm.seed_U = 'shuffle'; % seed of the node value sampling
+parm.mg = 1 ; % multigrid vs randomized path
+neigh.wradius = 3; % maximum range of neighboorhood search, factor of covar.range.
+neigh.lookup = false; % lookup table.
+neigh.nb = 40; % maximum number of neighborhood
+method = 'trad'; % SGS method: cst_par, cst_par_cond, hybrid, varcovar
+
+%% SGS
+[logPermM, t1] = SGS(nx, ny, nCase, covar, neigh, parm, method);
+[logPermF, t2] = SGS(nx, ny, nCase, covar, neigh, parm, method);
+covar.c0 = 1e8; % variance
+[EM_tot, t1] = SGS(nx, ny, nCase, covar, neigh, parm, method);
+covar.c0 = 1e6; % variance
+[EF_tot, t2] = SGS(nx, ny, nCase, covar, neigh, parm, method);
+
+i_index = rem([1:nx*ny]'-1, nx) + 1;
+j_index = floor(([1:nx*ny]'-1)/nx)+1;
+x_centroid = Lx/nx*(i_index-1/2);
+y_centroid = Ly/ny*(j_index-1/2);
+perm_m_tot = exp(reshape(logPermM, nx*ny, nCase));
+perm_f_tot = exp(reshape(logPermF, nx*ny, nCase));
+EM_tot = reshape(EM_tot, nx*ny, nCase) + 1e9; % mean: 1e9 var: 1e8 no log
+EF_tot = reshape(EF_tot, nx*ny, nCase) + 1e7;
+
+%%
+for i=1:nParallel
+    filename = ['perm_', num2str(i), '.mat'];
+    perm_f_ = perm_m_tot(:, (i-1)*nCase/nParallel+1:i*nCase/nParallel);
+    perm_m_ = perm_f_tot(:, (i-1)*nCase/nParallel+1:i*nCase/nParallel);
+    EM_ = EM_tot(:, (i-1)*nCase/nParallel+1:i*nCase/nParallel);
+    EF_ = EF_tot(:, (i-1)*nCase/nParallel+1:i*nCase/nParallel);
+    save(filename, 'perm_f_', 'perm_m_', 'EM_', 'EF_', 'i_index', 'j_index', 'x_centroid', 'y_centroid', 'nCase', 'nParallel');
+end
+% 
+% figure
+% perm1 = exp(logPermM(:,:,1));
+% pcolor(logPermM(:,:,1))
+% shading interp
+% colorbar
+% 
+% figure
+% perm2 = 100*exp(logPermF(:,:,1));
+% pcolor(logPermF(:,:,1))
+% shading interp
+% colorbar
